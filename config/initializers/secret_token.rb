4a8092ab0bc13610cb3e9d886c20b9b0688b89c2
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-19 15:28:47 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cymail::Application.config.secret_token = '911430645e3842ecf749a4a762aa34d4be52be8f59208bed53e4986ece0e4e899be5206bdf9f5a67fe17c2640d3a017d9dfb024bd981ace6a7e0c3869f6fbdd5'
