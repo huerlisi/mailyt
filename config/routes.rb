a6a921684f6977aa2797e489a884ea0d1022340b
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-20 09:25:26 +0200

    Use mails#index as root.

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Cymail::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "mails#index"
 
   # See how all your routes lay out with "rake routes"
 
