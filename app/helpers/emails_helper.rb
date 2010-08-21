3f9db811b1ce457382c5050bf64ea9890ccbf766
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-22 00:03:16 +0200

    Implement mail_address(email, name = nil) helper.

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -1,2 +1,9 @@
 module EmailsHelper
+  def mail_address(email, name = nil)
+    if name.blank?
+      return email
+    else
+      return "%s <%s>" % [name.strip, email.strip]
+    end
+  end
 end
--- spec/helpers/emails_helper_spec.rb
+++ spec/helpers/emails_helper_spec.rb
@@ -11,5 +11,30 @@ require 'spec_helper'
 #   end
 # end
 describe EmailsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "mail_address" do
+    it "shows name and email in <>" do
+      helper.mail_address("test@example.com", "Test").should == "Test <test@example.com>"
+    end
+    
+    it "shows only email if no name given" do
+      helper.mail_address("test@example.com").should == "test@example.com"
+    end
+
+    it "shows only email if name is blank" do
+      helper.mail_address("test@example.com", "").should == "test@example.com"
+      helper.mail_address("test@example.com", " ").should == "test@example.com"
+    end
+
+    it "strips surrounding whitespace from name" do
+      helper.mail_address("test@example.com", " Test ").should == "Test <test@example.com>"
+    end
+
+    it "leaves whitespace inside name intact" do
+      helper.mail_address("test@example.com", " Test Person ").should == "Test Person <test@example.com>"
+    end
+
+    it "strips surrounding whitespace from email" do
+      helper.mail_address(" test@example.com ", "Test").should == "Test <test@example.com>"
+    end
+  end
 end
