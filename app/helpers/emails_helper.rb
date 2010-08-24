746c05f752ee2fc74d72f5a00cb3c14c1ef83f86
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-24 09:11:28 +0200

    Show name, not email address in mail list.

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -6,4 +6,12 @@ module EmailsHelper
       return "%s <%s>" % [name.strip, email.strip]
     end
   end
+
+  def human_mail_address(email, name = nil)
+    if name.blank?
+      return email
+    else
+      return content_tag(:span, name.strip, :title => email.strip)
+    end
+  end
 end
