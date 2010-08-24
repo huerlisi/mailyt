9408b274a4261fb38e21c7c84c75a0f08b8a3d48
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-24 12:27:20 +0200

    Make whole email overview a link in lists.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+document.observe("dom:loaded", function() {
+  elements = $$("li[data-href]");
+  elements.each(function(element) {
+    element.style.cursor = "pointer";
+  });
+  
+  $(document.body).observe("click", function(event) {
+    var link = event.findElement("a");
+    if (link) {
+      return true;
+    };
+    
+    var element = event.findElement("li[data-href]");
+    if (element) {
+      var href = element.readAttribute('data-href');
+      document.location.href=href;
+
+      event.stop();
+      return false;
+    }
+  });
+});
+
 // Javascript Highlighter
 // Fixed version of comment in
 // http://stackoverflow.com/questions/1650389/prototype-js-highlight-words-dom-traversing-correctly-and-efficiently
