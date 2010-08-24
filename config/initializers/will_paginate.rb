c13d03be0fd5f848b98efdb6e3f829b8c7093156
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-24 10:20:03 +0200

    Always show pagination links.

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -2,3 +2,4 @@ WillPaginate::ViewHelpers.pagination_options[:previous_label] = 'Vorherige Seite
 WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Nächste Seite'
 WillPaginate::ViewHelpers.pagination_options[:class] = 'digg_pagination'
 WillPaginate::ViewHelpers.pagination_options[:renderer] = 'PaginationListLinkRenderer'
+WillPaginate::ViewHelpers.pagination_options[:show_always] = true
