b07ffbb734d805bffb9b0be137d24f2859909cba
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-25 20:17:20 +0200

    Support other encodings than utf8 in incomming mails.

--- app/mailers/basic.rb
+++ app/mailers/basic.rb
@@ -16,11 +16,11 @@ class Basic < ActionMailer::Base
      email = Email.new(:subject => mail.subject, :to => mail.to.join(', '), :from => mail.from.join(', '))
 
      if mail.multipart?
-       email.body = mail.parts[0].body.to_s
+       email.body = Iconv.conv('UTF-8', mail.parts[0].charset, mail.parts[0].body.to_s)
      else
        email.body = mail.body.to_s
      end
-
+     
      if mail.has_attachments?
        for attachment in mail.attachments
          a = email.attachments.build
