b304d002790e627fc5a2a896e03b4dbc433504c4
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-25 18:15:02 +0200

    Basic support for receiving mails.

--- app/mailers/basic.rb
+++ app/mailers/basic.rb
@@ -1,4 +1,5 @@
 class Basic < ActionMailer::Base
+  # Send text email based on Email object
   def text(email)
     @email = email
 
@@ -8,4 +9,22 @@ class Basic < ActionMailer::Base
     
     mail :to => email.to, :from => email.from, :subject => email.subject
   end
+
+  # Recive email
+   def receive(mail)
+     mail
+     email = Email.create(:subject => mail.subject, :to => mail.to.join(', '), :from => mail.from.join(', '))
+
+     if mail.multipart?
+       email.body = mail.parts[0].body.to_s
+     else
+       email.body = mail.body.to_s
+     end
+
+#     if mail.has_attachments?
+#       for attachment in mail.attachments
+#         email.attachments.create(:attachment => attachment)
+#       end
+#     end
+  end 
 end
