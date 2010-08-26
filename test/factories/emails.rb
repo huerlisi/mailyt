8121871b5d1f90ca492e586b0356093ab130d1d8
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-26 15:11:28 +0200

    Add Email factories.

--- test/factories/emails.rb
+++ test/factories/emails.rb
@@ -1,9 +1,12 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
+Factory.define :simple_email do |f|
+  f.from "from@example.com"
+  f.to "to@example.org"
+  f.subject "Simple mail"
+  f.date DateTime.parse("2001-01-01 08:15")
+  f.body "Some text"
+end
 
-Factory.define :email do |f|
-  f.from "MyString"
-  f.to "MyString"
-  f.subject "MyString"
-  f.date "MyString"
-  f.body "MyText"
+Factory.define :replied_email, :parent => :simple_email do |f|
+  f.subject "Original mail"
+  f.build_reply
 end
