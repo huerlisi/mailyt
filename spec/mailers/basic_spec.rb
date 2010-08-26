ad57deac81553c4ef20acc3b0b12628ce5a5489f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-26 10:02:48 +0200

    Fix basic mailer spec.

--- spec/mailers/basic_spec.rb
+++ spec/mailers/basic_spec.rb
@@ -2,16 +2,26 @@ require "spec_helper"
 
 describe Basic do
   describe "text" do
-    let(:mail) { Basic.text }
+    let(:email) {
+      email = mock_model(Email)
+      email.should_receive(:subject).and_return("Subject")
+      email.should_receive(:to).and_return("to@example.org")
+      email.should_receive(:from).and_return("from@example.com")
+      email.should_receive(:attachments).and_return([])
+      email.should_receive(:body).and_return("Body")
+
+      email
+    }
+    let(:mail) { Basic.text(email) }
 
     it "renders the headers" do
-      mail.subject.should eq("Text")
+      mail.subject.should eq("Subject")
       mail.to.should eq(["to@example.org"])
       mail.from.should eq(["from@example.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match("Body")
     end
   end
 
