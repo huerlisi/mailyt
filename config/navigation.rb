d4761b814e7616cdc0ee763210c4f50a165c42ed
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-26 15:54:19 +0200

    Protect all controllers/action; add login/logout navigation items.

--- config/navigation.rb
+++ config/navigation.rb
@@ -3,5 +3,7 @@ SimpleNavigation::Configuration.run do |navigation|
     # Mailyt navigation
     primary.item :emails, "Mail Journal", emails_path, :highlights_on => /\/emails(\/search.*)?$/
     primary.item :new_email, "Compose Mail", new_email_path, :highlights_on => /\/emails\/new/
+    primary.item :logout, "Logout", destroy_user_session_path, :highlights_on => /\/users\/sign_out/, :if => Proc.new { user_signed_in? }
+    primary.item :logout, "Login", new_user_session_path, :highlights_on => /\/users\/sign_in/, :unless => Proc.new { user_signed_in? }
   end
 end
