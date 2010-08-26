e23571b243116552782a1c5542e7b29dbc649dc6
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-26 11:35:21 +0200

    Spec date on new Email.

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Email do
-  specify { should be_valid }
-  
+  before { DateTime.stub!(:now).and_return(DateTime.parse('2001-01-01 08:15'))}
   context "when new" do
+    specify { should be_valid }
     specify { should_not be_reply }
+    its(:date) { should == "2001-01-01 08:15" }
   end
 end
