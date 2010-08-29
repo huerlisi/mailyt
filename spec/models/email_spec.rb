ef34dd80fcfa2921ca65184615e065f5b33125d7
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-08-29 12:15:16 +0200

    Fix specs as current user logic moved from model to controller.

--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -3,14 +3,13 @@ require 'spec_helper'
 describe Email do
   before {
     DateTime.stub!(:now).and_return(DateTime.parse('2001-01-01 08:15'))
-    @current_user = Factory(:user).make_current
   }
   
   context "when new" do
     specify { should be_valid }
     specify { should_not be_reply }
     its(:date) { should == "2001-01-01 08:15" }
-    its(:to_s) { should == "#{@current_user.email} -> : " }
+    its(:to_s) { should == " -> : " }
   end
 
   describe "#new" do
@@ -18,11 +17,6 @@ describe Email do
       Email.new.date.should == DateTime.now
     end
 
-    it "sets from to email of default user" do
-      @current_user.should_receive(:email).and_return("test@example.com")
-      Email.new.from == "test@example.com"
-    end
-    
     it "uses date from hash" do
       Email.new(:date => '2010-02-02').date.should == '2010-02-02'
     end
@@ -40,11 +34,6 @@ describe Email do
       email.should_receive(:in_reply_to).and_return(mock(Email))
       email.should be_reply
     end
-
-    it "sets from to email of default user" do
-      @current_user.should_receive(:email).and_return("test@example.com")
-      email.from == "test@example.com"
-    end
   end
   
   describe "email returned by #build_reply" do
@@ -56,6 +45,5 @@ describe Email do
     its(:in_reply_to) { should == original }
     its(:subject) { should == "Re: Original" }
     its(:to) { should == original.from }
-    its(:from) { should == @current_user.email }
   end
 end
