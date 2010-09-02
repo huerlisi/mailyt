5bbcca2c450c8260b32b9f7efd5b310b20ac46ac
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-09-02 09:25:48 +0200

    Provide instant feedback for email deletion and read/unread marking.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,26 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+// Speed up visual changes for some AJAX calls
+document.observe("dom:loaded", function() {
+  links = $$(".mark-read a");
+  links.each(function(link) {
+    link.observe("ajax:before", function(event) {
+      element = event.findElement('li.email');
+      element.toggleClassName('seen');
+      element.toggleClassName('unseen');
+    });
+  });
+
+  links = $$(".destroy a");
+  links.each(function(link) {
+    link.observe("ajax:before", function(event) {
+      element = event.findElement('li.email');
+      element.hide();
+    });
+  });
+});
+
 document.observe("dom:loaded", function() {
   elements = $$("li[data-href]");
   elements.each(function(element) {
