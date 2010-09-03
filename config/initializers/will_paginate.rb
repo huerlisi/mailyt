08ba691477c890bb9e923bf16208d0edd643aa55
Roman <roman.simecek@screenconcept.ch>
2010-09-03 14:01:20 +0200

    Magic encoding in will_paginate inserted.

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 WillPaginate::ViewHelpers.pagination_options[:previous_label] = 'Vorherige Seite'
 WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Nächste Seite'
 WillPaginate::ViewHelpers.pagination_options[:class] = 'digg_pagination'
